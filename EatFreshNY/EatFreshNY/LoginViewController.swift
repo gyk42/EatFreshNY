5fc3cdb083abd1b6b3496e2127bbe2ad54f7611b
Grace Yu <gyk42@hotmail.com>
2017-01-15 15:24:23 -0500

    Add outlets to textfields and actions to buttons

--- EatFreshNY/EatFreshNY/LoginViewController.swift
+++ EatFreshNY/EatFreshNY/LoginViewController.swift
@@ -9,27 +9,21 @@
 import UIKit
 
 class LoginViewController: UIViewController {
+   
+   // MARK: IBOutlet -------------------------------
+   
+   @IBOutlet weak var clientNameTextField: UITextField!
+   
+   @IBOutlet weak var clientNamePassword: UITextField!
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+   
+   // MARK: IBAction -------------------------------
+   
+   @IBAction func loginBtnPressed(_ sender: Any) {
+   
+   }
+   
 }
--- EatFreshNY/EatFreshNY/VendorSignUpViewController.swift
+++ EatFreshNY/EatFreshNY/VendorSignUpViewController.swift
@@ -9,27 +9,31 @@
 import UIKit
 
 class VendorSignUpViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+   
+   //MARK: IBOutlet ------------------------------
+   @IBOutlet weak var vendorLogo: UIImageView!
+   
+   @IBOutlet weak var vendorName: UITextField!
+   
+   @IBOutlet weak var vendorAddress: UITextField!
+   
+   @IBOutlet weak var vendorContactEmail: UITextField!
+   
+   @IBOutlet weak var vendorPhoneNumber: UITextField!
+   
+   @IBOutlet weak var vendorWebsite: UITextField!
+   
+   override func viewDidLoad() {
+      super.viewDidLoad()
+   }
+   
+   //MARK: IBAction ------------------------------
+   
+   @IBAction func changeLogoPressed(_ sender: Any) {
+   
+   }
+   
+   @IBAction func signUpPressed(_ sender: Any) {
+   
+   }
 }
