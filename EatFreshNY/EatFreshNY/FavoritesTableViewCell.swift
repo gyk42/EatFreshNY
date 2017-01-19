98e75c1500cddf2a9d395eee713707979e15048e
Matt Tripodi <mtripodi1027@gmail.com>
2017-01-19 16:30:56 -0500

    Created favorites tab / ViewController

---
+++ EatFreshNY/EatFreshNY/FavoritesTableViewCell.swift
@@ -0,0 +1,29 @@
+//
+//  FavoritesTableViewCell.swift
+//  EatFreshNY
+//
+//  Created by Matt Tripodi on 1/19/17.
+//  Copyright © 2017 Grace Yu. All rights reserved.
+//
+
+import UIKit
+
+class FavoritesTableViewCell: UITableViewCell {
+	
+	// MARK: IBOutlets -----------------------------------------------------------
+	@IBOutlet weak var favoriteImage: UIImageView!
+	@IBOutlet weak var favoriteVendorLabel: UILabel!
+	
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
