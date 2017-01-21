0a3ee21940ea7f8a1c7697b6c0f475a5345fcd48
Matt Tripodi <mtripodi1027@gmail.com>
2017-01-21 17:53:57 -0500

    Working on data persistence in the cart

--- EatFreshNY/EatFreshNY/CartViewController.swift
+++ EatFreshNY/EatFreshNY/CartViewController.swift
@@ -10,23 +10,23 @@ import UIKit
 
 class CartViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
    
-   // IBOutlets
+   // MARK: IBOutlets --------------------------------------------------------------------------------------
    @IBOutlet weak var cartTableView: UITableView!
    @IBOutlet weak var cartNumberOfItemsTotalLabel: UILabel!
    @IBOutlet weak var cartTotalPriceLabel: UILabel!
    
-   // IBActions
+   // MARK: IBActions ----------------------------------------------------------------------------------------
    @IBAction func purchaseButtonTapped(_ sender: Any) {
    }
-   
+	
+	// MARK: ViewDidLoad ---------------------------------------------------------------------------------------
    override func viewDidLoad() {
       super.viewDidLoad()
-      
-      // Do any additional setup after loading the view.
+		
+		
    }
    
-   // TableView
-   
+   // MARK: TableView ------------------------------------------------------------------------------------------
    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
       return 1
    }
@@ -36,20 +36,5 @@ class CartViewController: UIViewController, UITableViewDelegate, UITableViewData
       return cell
    }
    
-   override func didReceiveMemoryWarning() {
-      super.didReceiveMemoryWarning()
-      // Dispose of any resources that can be recreated.
-   }
-   
-   
-   /*
-    // MARK: - Navigation
-    
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-    // Get the new view controller using segue.destinationViewController.
-    // Pass the selected object to the new view controller.
-    }
-    */
-   
+ 
 }
--- EatFreshNY/EatFreshNY/ProductDetailPageViewController.swift
+++ EatFreshNY/EatFreshNY/ProductDetailPageViewController.swift
@@ -14,14 +14,17 @@ import FirebaseStorage
 class ProductDetailPageViewController: UIViewController {
    
    var product: Product?
+	
+	var cartInfo: String?
    
-   // MARK: IBOutlets ----------------------------------
+   // MARK: IBOutlets --------------------------------------------------------------------
    @IBOutlet weak var productDetailImage: UIImageView!
    @IBOutlet weak var productNameLabel: UILabel!
    @IBOutlet weak var productDescriptionLabel: UILabel!
    @IBOutlet weak var productPriceLabel: UILabel!
    @IBOutlet weak var quantityNumberLabel: UILabel!
-   
+	
+	// MARK: ViewDidLoad -------------------------------------------------------------------
    override func viewDidLoad() {
       super.viewDidLoad()
       
@@ -34,12 +37,25 @@ class ProductDetailPageViewController: UIViewController {
    }
    
    
-   // MARK: IBActions ----------------------------------
-   
+   // MARK: IBActions ------------------------------------------------------------------------
    @IBAction func addToCartButtonTapped(_ sender: UIButton) {
+		
+		CartModel.shared.Key = NSUUID().uuidString // creates a randome string to  be uses as cartID
+		CartModel.Item(productName: productNameLabel.text!,
+		               productPrice: productPriceLabel.text!,
+		               productQuantity: quantityNumberLabel.text!,
+		               productPhoto: productDetailImage.image!,
+		               productId: product!)
+		
+		
+		var cartArray = [productNameLabel.text, productDescriptionLabel.text, productPriceLabel.text, quantityNumberLabel.text]
+		NSKeyedArchiver.archiveRootObject(cartArray, toFile: CartViewController!)
+ 
       
    }
-   
+	
+	
+   // MARK: IBAction Buttons --------------------------------------------------------------------
    @IBAction func quantityStepperTapped(_ sender: Any) {
       
    }
