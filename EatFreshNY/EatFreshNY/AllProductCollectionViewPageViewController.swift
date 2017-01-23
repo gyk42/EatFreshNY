6fb584f085e8a0685d1b5c0463615666722e9197
Richel Cuyler <richelcuyler@icloud.com>
2017-01-23 16:58:27 -0500

    Updated Vendor Flow minus UI

--- EatFreshNY/EatFreshNY/AllProductCollectionViewPageViewController.swift
+++ EatFreshNY/EatFreshNY/AllProductCollectionViewPageViewController.swift
@@ -56,6 +56,8 @@ class AllProductCollectionViewPageViewController: UIViewController, UICollection
          }
       })
    }
+    
+    
    
    // CollectionView
    
--- EatFreshNY/EatFreshNY/InventoryProductViewController.swift
+++ EatFreshNY/EatFreshNY/InventoryProductViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 import Firebase
 
 class InventoryProductViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
-
+    
     
     var ref: FIRDatabaseReference!
     static var userID = FIRAuth.auth()!.currentUser!.uid
@@ -23,15 +23,21 @@ class InventoryProductViewController: UIViewController, UITableViewDataSource, U
     override func viewDidLoad() {
         super.viewDidLoad()
         productTableviewDisplay()
-        // Do any additional setup after loading the view.
+        
     }
+    
+    
+    
+    //MARK: Display Tableview with Vendor's Products
+    
     func productTableviewDisplay() {
         let productsRef = FIRDatabase.database().reference(withPath:"products")
         let productID = productsRef.ref.key
         let productsQuery = productsRef.queryOrdered(byChild: "userID").queryEqual(toValue: InventoryProductViewController.userID)
         
-        productsQuery.observeSingleEvent(of: .value, with: { (snapshot) in
+        productsQuery.observe(.value, with: { (snapshot) in
             
+            self.products.removeAll()
             for product in snapshot.children {
                 
                 let product = Product(snapshot: product as! FIRDataSnapshot)
@@ -44,7 +50,7 @@ class InventoryProductViewController: UIViewController, UITableViewDataSource, U
         })
     }
     
-    // MARK: - Table view data source
+    // MARK: - Tableview Data Source
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return products.count
     }
@@ -52,18 +58,37 @@ class InventoryProductViewController: UIViewController, UITableViewDataSource, U
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "inventoryCell", for: indexPath) as! InventoryProductTableViewCell
-        //        var imageName = ""
-        //        imageName = NSUUID().uuidString
         cell.productName.text = products[indexPath.row].name
         cell.productPrice.text = products[indexPath.row].price
         cell.productQuantity.text = products[indexPath.row].quantity
         cell.productDescription.text = products[indexPath.row].description
         cell.productMarketDate.text = products[indexPath.row].marketDate
-        //cell.productImage.image = products[indexPath.row].image
-        
+        //ADD IMAGE TO TABLEVIEW - RICHEL & OSKAR
         return cell
     }
+    
+    func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            ProductModel.shared.deleteProduct(product: products[indexPath.row])            
+        }
+    }
+    
+    
+    //MARK: Update Product Values
+    
+    @IBAction func updateProductValues(_ sender: UIButton) {
 
- 
+    }
+    
+    
+    func populateProduct() {
 
+    }
+    
+    
+    
 }
+
+
+
+    
